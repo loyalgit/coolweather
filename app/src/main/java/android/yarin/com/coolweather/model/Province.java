0c2e669c485300266ab6eb98e14146154dc2beb2
loyalgit <791379061@qq.com>
2016-02-03 17:05:46 +0800

    新增数据库帮助类,以及各表对应的实体类。

---
+++ app/src/main/java/android/yarin/com/coolweather/model/Province.java
@@ -0,0 +1,29 @@
+package android.yarin.com.coolweather.model;
+
+/**
+ * Created by Michael on 16/2/3.
+ */
+public class Province {
+	private int id;
+	private String provinceName;
+	private String provinceCode;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getProvinceName() {
+		return provinceName;
+	}
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+	public String getProvinceCode() {
+		return provinceCode;
+	}
+	public void setProvinceCode(String provinceCode) {
+		this.provinceCode = provinceCode;
+	}
+}
